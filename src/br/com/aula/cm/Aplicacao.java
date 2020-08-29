0488d33dc2742d800fddc8c102b14ecfb5327026
Franciscoas4 <franciscoas4@gmail.com>
2020-08-28 22:10:41 -0300

    Criação do ciclo do jogo e captura das informações do usuário

--- src/br/com/aula/cm/Aplicacao.java
+++ src/br/com/aula/cm/Aplicacao.java
@@ -1,17 +1,13 @@
 package br.com.aula.cm;
 
 import br.com.aula.cm.modelo.Tabuleiro;
+import br.com.aula.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
+		new TabuleiroConsole(tabuleiro);
 	}
 }
